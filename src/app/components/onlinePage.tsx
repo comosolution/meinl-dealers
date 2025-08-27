da41cf84fa8d0536735aa61b67f002478122bf03
E R I C S C H M I D T <info@ericschmidt.de>
2025-08-27 14:24:22 +0200

    tweak online page

--- src/app/components/onlinePage.tsx
+++ src/app/components/onlinePage.tsx
@@ -8,14 +8,14 @@ export default function OnlinePage() {
 
   return (
     <div className="mt-20 p-4">
-      <div className="fixed left-4 top-24 flex flex-col gap-1 text-sm">
+      <div className="fixed left-4 top-1/2 -translate-y-1/2 flex flex-col gap-1 text-sm">
         {existingLetters.map((letter) => (
           <button
             key={letter}
             onClick={() => {
               const el = document.getElementById(`letter-${letter}`);
               if (el) {
-                el.scrollIntoView({ behavior: "smooth", block: "start" });
+                el.scrollIntoView({ behavior: "smooth", block: "center" });
               }
             }}
             className="text-gray-400 hover:text-black cursor-pointer"
@@ -24,14 +24,14 @@ export default function OnlinePage() {
           </button>
         ))}
       </div>
-      <main className="grid grid-cols-3 gap-2 ml-12">
+      <main className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 ml-12">
         {online.map((shop, index) => (
           <a
             key={index}
             id={`letter-${shop.title[0].toUpperCase()}`}
             href={shop.website}
             target="_blank"
-            className="flex flex-col items-center px-4 py-8 border border-transparent group hover:border-black/20"
+            className="flex flex-col items-center gap-2 px-4 py-8 border border-transparent group hover:border-black/20"
           >
             <div
               className="relative overflow-hidden"
@@ -45,7 +45,7 @@ export default function OnlinePage() {
                 className="grayscale-100 group-hover:grayscale-0 transition-all"
               />
             </div>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center text-center">
               <h2>{shop.title}</h2>
               <p className="text-xs dimmed">
                 {shop.address} {shop.housenumber}, {shop.zip} {shop.city}
