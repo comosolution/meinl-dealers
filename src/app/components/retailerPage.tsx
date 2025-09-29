97d579a1747394dd937753bc8e9584ae32c7cdd6
E R I C S C H M I D T <info@ericschmidt.de>
2025-09-29 09:27:55 +0200

    tweak

--- src/app/components/retailerPage.tsx
+++ src/app/components/retailerPage.tsx
@@ -301,7 +301,7 @@ export default function RetailerPage() {
         </div>
         {showSearchButton && (
           <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-30">
-            <Button size="xs" onClick={handleAreaSubmit}>
+            <Button size="xs" color="black" onClick={handleAreaSubmit}>
               Search this area
             </Button>
           </div>
