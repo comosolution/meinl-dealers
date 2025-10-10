3c3dfaf7b0b95ed0b06f8929a790ce19a133b369
E R I C S C H M I D T <info@ericschmidt.de>
2025-10-10 10:26:30 +0200

    fix button z-index

--- src/app/components/retailerPage.tsx
+++ src/app/components/retailerPage.tsx
@@ -364,7 +364,7 @@ export default function RetailerPage() {
           </ActionIcon>
         </div>
         {showSearchButton && (
-          <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-30">
+          <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-20">
             <Button size="xs" color="black" onClick={handleAreaSubmit}>
               Search this area
             </Button>
