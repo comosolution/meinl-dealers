7e36f72b018da24c64957650a621a19545a5867d
E R I C S C H M I D T <info@ericschmidt.de>
2025-11-10 15:12:40 +0100

    update api url

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -1,1 +1,4 @@
-export const MEINL_WEB_API = "https://apidev.meinl.de/MeinlWebApi/api";
+export const MEINL_WEB_API =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+    ? "https://apidev.meinl.de/MeinlWebApi/api"
+    : "https://api.meinl.loc/MeinlWebApi/api";
