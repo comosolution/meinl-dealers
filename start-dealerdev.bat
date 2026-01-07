f1b2d119ef56d43bb1001bee8fe1cc950be99ed7
E R I C S C H M I D T <info@ericschmidt.de>
2026-01-07 14:51:14 +0100

    update start scripts

--- start-dealerdev.bat
+++ start-dealerdev.bat
@@ -1,3 +1,5 @@
 @echo off
 cd /d C:\inetpub\dealerdev.meinl.loc
+
+set NODE_OPTIONS=--use-system-ca
 npm run start -- --port 3001
--- start-dealerprod.bat
+++ start-dealerprod.bat
@@ -1,3 +1,5 @@
 @echo off
 cd /d C:\inetpub\dealer.meinl.de
+
+set NODE_OPTIONS=--use-system-ca
 npm run start -- --port 3001
